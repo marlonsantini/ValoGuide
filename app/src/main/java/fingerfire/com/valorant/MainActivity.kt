a3fa1557cb2126082fad9304dc34223e9b2c7200
Marlon Santini <marlonsantini@gmail.com>
2023-02-06 19:40:06 -0300

    Initial commit

---
+++ app/src/main/java/fingerfire/com/valorant/MainActivity.kt
@@ -0,0 +1,18 @@
+package fingerfire.com.valorant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fingerfire.com.valorant.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/fingerfire/com/valorant/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package fingerfire.com.valorant.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
