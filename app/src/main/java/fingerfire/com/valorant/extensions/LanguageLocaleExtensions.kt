089ae835e97143b35dc753f97fd0f4cebf097cd3
Marlon Santini <marlonsantini@gmail.com>
2023-05-02 01:35:27 -0300

    fix bugs

--- app/src/main/java/fingerfire/com/valorant/extensions/LanguageLocaleExtensions.kt
+++ app/src/main/java/fingerfire/com/valorant/extensions/LanguageLocaleExtensions.kt
@@ -3,5 +3,26 @@ package fingerfire.com.valorant.extensions
 import java.util.*
 
 fun getAppLanguage(): String {
-    return Locale.getDefault().toLanguageTag()
+    var locale = Locale.getDefault().toLanguageTag()
+
+    val validLocales = listOf(
+        "ar-AE", "de-DE", "en-US", "es-ES", "es-MX", "fr-FR", "id-ID", "it-IT",
+        "ja-JP", "ko-KR", "pl-PL", "pt-BR", "ru-RU", "th-TH", "tr-TR", "vi-VN", "zh-CN", "zh-TW"
+    )
+
+    if(locale.startsWith("es")) {
+        locale = "es-ES"
+    } else if (locale.startsWith("en")) {
+        locale = "en-US"
+    } else if (locale.startsWith("de")) {
+        locale = "de-DE"
+    } else if (locale.startsWith("fr")) {
+        locale = "fr-FR"
+    } else if (locale.startsWith("zh")) {
+        locale = "zh-CN"
+    } else if (locale !in validLocales){
+        locale = "en-US"
+    }
+
+    return locale
 }
